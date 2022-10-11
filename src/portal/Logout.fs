ddba97609f69b9bb30fae353c2e440c7e5aa05e9
b0wter <b0wter@gmx.de>
2022-10-11 13:46:44 +0200

    Fixed 204 content error for logout

--- src/portal/Logout.fs
+++ src/portal/Logout.fs
@@ -10,14 +10,14 @@ open Microsoft.AspNetCore.Http
 module Logout =
     
     let handler (organization: Organization.Organization, token: string) =
-        fun (_: HttpFunc) (ctx: HttpContext) ->
+        fun (next: HttpFunc) (ctx: HttpContext) ->
             asyncResult {
                 if organization.AccessTokens |> List.exists (fun at -> at.Token = token) then
                     let updatedOrg = { organization with AccessTokens = organization.AccessTokens |> List.where (fun at -> at.Token <> token) }
                     let repo = ctx.GetService<CouchDb.C>()
                     let! _ = repo.SaveOrganization updatedOrg
                     do ctx.SetStatusCode 204
-                    return! ctx.WriteStringAsync System.String.Empty
+                    return! next ctx
                 else
                     do ctx.SetStatusCode 500
                     return! ctx.WriteStringAsync "Internal server error: the given token could be used to login but is unknown to the organization"
